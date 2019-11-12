d9905a3c68d03d380b56ffb963703f6a5935d987
Barsha <mebarsha1@gmail.com>
2019-11-12 09:36:48 +0545

    display

---
+++ day4.py
@@ -0,0 +1,12 @@
+biology=int(input("marks of biology"))
+chemistry=int(input("marks of chemistry"))
+physics=int(input("marks of physics"))
+nepali=int(input("marks of nepali"))
+math=int(input("marks of math"))
+list=[biology,chemistry,physics,nepali,math]
+print(list)
+sum=(biology+chemistry+physics+nepali+math)
+print(sum)
+
+user=input("enter the string")
+print(user.count(input("enter string to be counted")))
